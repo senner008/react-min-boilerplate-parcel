63b1c56b50d00a30f30f4f1dd489ceece31557da
nig <nig@beepanalytics.com>
2020-01-24 15:38:43 +0100

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// babel-polyfill is used to enable async/await
+import "babel-polyfill";
+
+ReactDOM.render(
+    <h1>Hello World</h1>, 
+    document.getElementById("root")
+);
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Allow insecure localhost:
+ - chrome://flags/#allow-insecure-localhost (in chrome address bar)
